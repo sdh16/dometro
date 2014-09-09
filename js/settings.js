b64ce4958d9875027ffefa317152db82736ee018
sdh16 <sdh16@users.noreply.github.com>
2014-09-10 04:05:40 +0530

    spme more work on settings

--- js/settings.js
+++ js/settings.js
@@ -115,7 +115,7 @@
 	  $('#uservariablesedittable #uservariabledelete').attr("class", "button-dis");
 
     $.varNames = [];	
-    var oTable = $('#uservariablestable').dataTable();
+    var oTable = $('#Variables-table').dataTable();
     oTable.fnClearTable();
     $.ajax({
        url: "/json.htm?type=command&param=getuservariables",
@@ -166,50 +166,203 @@
 
   ShowUserVariables = function()
   {
-	  var oTable = $('#uservariablestable').dataTable( {
-	    "sDom": '<"H"lfrC>t<"F"ip>',
-	    "oTableTools": {
-		  "sRowSelect": "single",
-	    },
-	    "fnDrawCallback": function (oSettings) {
-          var nTrs = this.fnGetNodes();
-          $(nTrs).click(
-			  function(){
-				  $(nTrs).removeClass('row_selected');
-				  $(this).addClass('row_selected');
-				  $('#uservariablesedittable #uservariableupdate').attr("class", "bg-darkRed fg-white");
-				  $('#uservariablesedittable #uservariabledelete').attr("class", "bg-darkRed fg-white");
-				  var anSelected = fnGetSelected( oTable );
-				  if ( anSelected.length !== 0 ) {
-					  var data = oTable.fnGetData( anSelected[0] );
-					  var idx= data["DT_RowId"];
-					  $.userVariableIdx=idx;	
-					  $("#uservariablesedittable #uservariableupdate").attr("href", "javascript:AddLink('u')");
-					  $("#uservariablesedittable #uservariabledelete").attr("href", "javascript:DeleteVariable(" + idx + ")");
-					  $("#uservariablesedittable #uservariablename").val(data["0"]);
-					  $("#uservariablesedittable #uservariabletype").val(data["DT_ItemType"]);
-					  $("#uservariablesedittable #uservariablevalue").val(data["2"]);
-				  }
-		  });
-	    },    
-	    "aaSorting": [[ 0, "desc" ]],
-	    "bSortClasses": false,
-	    "bProcessing": true,
-	    "bStateSave": true,
-	    "bJQueryUI": true,
-	    "iDisplayLength" : 10,
-	    'bLengthChange': false,
-	    "sPaginationType": "full_numbers"
-	  } );
-	
-	  RefreshUserVariablesTable()
+  
+      
+    $("<table></table>")
+      .attr("id", "Variables-table")
+      .appendTo("#fibaromain")
+      .addClass("table striped hovered dataTable")
+      .attr("border","0")
+      .attr("cellpadding","0")
+      .attr("cellspacing","20")
+    
+    $("<thead><thead")
+      .attr("id","Variables-table-thead")
+      .appendTo("#Variables-table")
+      .addClass("text-left")
+    $("<tr><tr")
+      .attr("id","Variables-table-thead-row")
+      .appendTo("#Variables-table-thead")
+    $("<th></th>")
+      .appendTo("#Variables-table-thead-row")
+      .text("Variable name")
+    $("<th></th>")
+      .appendTo("#Variables-table-thead-row")
+      .text("Variable type")
+    $("<th></th>")
+      .appendTo("#Variables-table-thead-row")
+      .text("Current value")
+    $("<th></th>")
+      .appendTo("#Variables-table-thead-row")
+      .text("Last update")
+    var oTable = $('#Variables-table').dataTable( {
+      "sDom": '<"H"lfrC>t<"F"ip>',
+      "oTableTools": {
+      "sRowSelect": "single",
+    },
+    "fnDrawCallback": function (oSettings) {
+       var nTrs = this.fnGetNodes();
+       $(nTrs).click(
+       function(){
+	  $(nTrs).removeClass('row_selected');
+	  $(this).addClass('row_selected');
+	  $('#uservariablesedittable #uservariableupdate').attr("class", "bg-darkRed fg-white");
+	  $('#uservariablesedittable #uservariabledelete').attr("class", "bg-darkRed fg-white");
+	  var anSelected = fnGetSelected( oTable );
+	  if ( anSelected.length !== 0 ) {
+		  var data = oTable.fnGetData( anSelected[0] );
+		  var idx= data["DT_RowId"];
+		  $.userVariableIdx=idx;	
+		  $("#uservariablesedittable #uservariableupdate").attr("href", "javascript:AddLink('u')");
+		  $("#uservariablesedittable #uservariabledelete").attr("href", "javascript:DeleteVariable(" + idx + ")");
+		  $("#uservariablesedittable #uservariablename").val(data["0"]);
+		  $("#uservariablesedittable #uservariabletype").val(data["DT_ItemType"]);
+		  $("#uservariablesedittable #uservariablevalue").val(data["2"]);
+	  }
+        });
+      },    
+      "aaSorting": [[ 0, "desc" ]],
+      "bSortClasses": false,
+      "bProcessing": true,
+      "bStateSave": true,
+      "bJQueryUI": true,
+      "iDisplayLength" : 10,
+      'bLengthChange': false,
+      "sPaginationType": "full_numbers"
+    } );
+    RefreshUserVariablesTable()
+    
+    $("<h2></h2>")
+    .attr("id", "Edit-variables")
+    .appendTo("#fibaromain")
+    .text("Edit variable")
+    
+    $("<table></table>")
+      .attr("id", "uservariablesedittable")
+      .appendTo("#fibaromain")
+      .addClass("table")
+      
+    $("<tr><tr")
+      .attr("id","uservariablenamerow")
+      .appendTo("#uservariablesedittable")      
+    $("<td><td")
+      .attr("id","uservariablenamelabel")
+      .appendTo("#uservariablenamerow")
+      .attr("align", "right")
+      .attr("style", "width:200px")
+    $("<label><label>")
+      .appendTo("#uservariablenamelabel")
+      .attr("for", "uservariablename")
+      .text("Variable name")
+    $("<td><td")
+      .attr("id","uservariablename")
+      .appendTo("#uservariablenamerow")
+    $("<input><input")
+      .attr("id","uservariablenameinput")
+      .appendTo("#uservariablenamerow")
+      .attr("style", "width: 250px; padding: .2em;")
+      .addClass("text ui-widget-content ui-corner-all")
+      
+    $("<tr><tr")
+      .attr("id","uservariabletyperow")
+      .appendTo("#uservariablesedittable")      
+    $("<td><td")
+      .attr("id","uservariabletypelabel")
+      .appendTo("#uservariabletyperow")
+      .attr("align", "right")
+      .attr("style", "width:200px")
+    $("<label><label>")
+      .appendTo("#uservariabletypelabel")
+      .text("Variable type")
+    $("<td><td")
+      .attr("id","uservariabletypeinput")
+      .appendTo("#uservariabletyperow")
+    $("<select><select")
+      .attr("id","uservariabletypeselect")
+      .appendTo("#uservariabletyperow")
+      .attr("style", "width: 250px; padding: .2em;")
+      .addClass("text ui-widget-content ui-corner-all")
+    $("<option><option")
+      .appendTo("#uservariabletypeselect")
+      .attr("value", "0")
+      .text("Integer")
+    $("<option><option")
+      .appendTo("#uservariabletypeselect")
+      .attr("value", "1")
+      .text("Float")
+    $("<option><option")
+      .appendTo("#uservariabletypeselect")
+      .attr("value", "2")
+      .text("String")
+    $("<option><option")
+      .appendTo("#uservariabletypeselect")
+      .attr("value", "3")
+      .text("Date")
+    $("<option><option")
+      .appendTo("#uservariabletypeselect")
+      .attr("value", "4")
+      .text("Time")
+
+
+    $("<tr><tr")
+      .attr("id","uservariablevaluerow")
+      .appendTo("#uservariablesedittable")      
+    $("<td><td")
+      .attr("id","uservariablevaluelabel")
+      .appendTo("#uservariablevaluerow")
+      .attr("align", "right")
+      .attr("style", "width:200px")
+    $("<label><label>")
+      .appendTo("#uservariablevaluelabel")
+      .text("Variable value")
+    $("<td><td")
+      .attr("id","uservariablevalueinput")
+      .appendTo("#uservariablevaluerow")
+    $("<input><input")
+      .attr("id","uservariablencalueinputdata")
+      .appendTo("#uservariablevaluerow")
+      .attr("style", "width: 250px; padding: .2em;")
+      .addClass("text ui-widget-content ui-corner-all")
+      
+/*    
+	<table class="display" id="uservariablesedittable" border="0" cellpadding="0" cellspacing="20">
+		<tr id="uservariablenamerow">
+			<td align="right" style="width:110px"><label for="uservariablename"><span>Variable name</span>:</label></td>
+			<td><input type="text" id="uservariablename" style="width: 250px; padding: .2em;" class="text ui-widget-content ui-corner-all" /></td>
+		</tr>
+		<tr>
+			<td align="right" style="width:110px"><label for="uservariabletype"><span>Variable type</span>:</label></td>
+			<td><select id="uservariabletype" style="width:160px" class="combobox ui-corner-all">
+					<option value="0" data-i18n="Integer">Integer</option>
+					<option value="1" data-i18n="Float">Float</option>
+					<option value="2" data-i18n="String">String</option>
+					<option value="3" data-i18n="Date">Date</option>
+					<option value="4" data-i18n="Time">Time</option>
+				</select>
+			</td>
+		</tr>
+		<tr id="uservariablevaluerow">
+			<td align="right" style="width:110px"><label for="uservariablevalue"><span>Variable value</span>:</label></td>
+			<td><input type="text" id="uservariablevalue" style="width: 250px; padding: .2em;" class="text ui-widget-content ui-corner-all" /></td>
+		</tr>
+
+		<tr>
+			<td colspan="2">
+				<button class="bg-darkRed fg-white" onclick="AddLink('a');"">Add</button>
+				<button class="bg-darkRed fg-white" id="uservariableupdate">Update</button>&nbsp;&nbsp;&nbsp;
+				<button class="bg-darkRed fg-white" id="uservariabledelete">Delete</button>
+			</td>
+		</tr>	
+	</table>
+*/    
+    
+
   }
   
 }(jQuery, window, document));
 
 $(document).ready(function() {
   $.userVariableIdx=0;
-	$.varNames = [];
-	ShowUserVariables();
-
+  $.varNames = [];
+  ShowUserVariables();
 });
